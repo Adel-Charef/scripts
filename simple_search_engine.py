5310aea2121b950fe96f9ddc507b1a1e32008752
Charef Adel <charefadel23@tutanota.com>
2020-09-27 14:51:37 +0000

    added a simple search engine in python

---
+++ simple_search_engine.py
@@ -0,0 +1,44 @@
+#! usr/bin/python3
+# Open sevral google search results in new tabs
+
+import sys, requests, webbrowser
+from bs4 import BeautifulSoup
+
+query = sys.argv[1:]
+URL = f"https://google.com/search?q={query}"
+
+user_agent = "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"
+
+headers = {"user-agent": user_agent}
+
+print("""
+	 *****************************************
+         |            My Search Engine           |
+         *****************************************
+         """)
+         
+response = requests.get(URL, headers=headers)
+print("Googling.....\n")
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, "html.parser")
+else:
+    print("The Status Code is:", response.status_code)
+
+results = []
+
+for g in soup.find_all("div", class_="r"):
+    anchors = g.find_all("a")
+    if anchors:
+        links = anchors[0]["href"]
+        results.append(links)
+
+print("*****************************************")
+print("              Results........            ")
+print("*****************************************\n")
+for i in range(min(8 ,len(results))):
+    print(f"{i}-- {results[i]}\n")
+selected_result = int(input(">> -Select a Result: "))
+webbrowser.open(results[selected_result])
+    
+    
